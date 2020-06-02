dd018e8e9f1a0ea9afebc4ae9e99611fa252bb86
Hrithik Jha <hrithik.jha2017@vitstudent.ac.in>
2020-06-02 21:05:57 +0530

    Add model testing

--- test/local_vis.py
+++ test/local_vis.py
@@ -4,81 +4,73 @@ import os
 import requests
 import json
 import datetime
-#import thread, winsound
 
-face_cascade = cv2.CascadeClassifier('haarcascades\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascades\haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('..\\haarcascades\\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('..\\haarcascades\\haarcascade_eye.xml')
 
-'''
-VideoCapture with a sequence of images only works for images that are numbered consecutively starting at 0. 
-It's intended as a convenience. Since your images aren't numbered like that, it's no longer a convenient to use VideoCapture. 
-Simply use glob to get a list of files, sort it if necessary, and read the frames with imread in a loop.
-'''
-
-files = os.listdir('./img')
+#files = os.listdir('./img')
 count = 0
 iters = 0
-lenOfFiles = len(files)
 
-'''
-ADD ARGUMENT FUNCTIONALITY FOR ID AND TIME STAMP
-'''
-
-# Add arguments for ID and Status
+# Sending info to main server
 def sendPutRequest(idee, timestamp, isDrowsy):
-  payload = {
-	  "id" : idee,
-    "time": timestamp,
-    "isDrowsy": isDrowsy
-  }
-  
-  #headers['content-type'] = 'application/json'
+    payload = {
+        "id": idee,
+        "time": timestamp,
+        "isDrowsy": isDrowsy
+    }
+    #headers['content-type'] = 'application/json'
+    print(payload)
+    r = requests.put("https://driver-drowsiness.herokuapp.com/addActivity",
+                     data=json.dumps(payload), headers={'Content-Type': 'application/json'})
+    print(r)
 
-  r = requests.put("https://driver-drowsiness.herokuapp.com/addActivity", data = json.dumps(payload), headers={'Content-Type': 'application/json'})
-  #requests.post(url, data=raw_data, headers={'Content-Type': 'application/x-www-form-urlencoded'})
-  print(r)
+#lenOfFiles = len(files)
+cam = cv2.VideoCapture('./drowsyVideo_Trim.mp4')
+while(cam.isOpened()):
+    #files = os.listdir('./img')
+    #status = False
+    #if len(files) == 0:
+    #    continue
+    #elif len(files) == lenOfFiles:
+    #    continue
 
-while(True):
-  files = os.listdir('./img')
-  status = False
-  if len(files) == 0:
-    continue
-  elif len(files) == lenOfFile:
-    continue
-  
-  lenOfFile = len(files)
-  status = False
-  
-  cam = cv2.VideoCapture('./img/' + files[-1])
-  ret, cur = cam.read()
-  # Experimental
-  #r = requests.get("http://localhost:5000/test")
-  gray = cv2.cvtColor(cur, cv2.COLOR_BGR2GRAY)
-  faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors=1, minSize=(10,10))
-  for (x,y,w,h) in faces:
-  	#cv2.rectangle(cur,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h,x:x+w]
-    roi_color = cur[y:y+h,x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    if len(eyes) == 0:
-      print ("Eyes closed")
-    else:
-      print ("Eyes open")
-    count += len(eyes)
-    iters += 1
-    if iters == 2:
-      iters = 0
-      if count == 0:
-        print ("Drowsiness Detected!")
-        status = True
-        #thread.start_new_thread(beep,())
-      count = 0
-  #sendPutRequest('LXklRS2NhL5aWVsvxCGn', str(datetime.datetime.now()), status)
-    #for (ex,ey,ew,eh) in eyes:
-    #	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh), (0,255,0),2)
-  #cv2.imshow('frame', cur)
-  # Debug sending requests
+    #print(files[0])
 
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    cv2.destroyAllWindows()
-    break
+    #lenOfFile = len(files)
+    status = False
+
+    #cam = cv2.VideoCapture('./drowsyVideo.mp4')
+    ret, cur = cam.read()
+    cv2.imshow('frame', cur) 
+    gray = cv2.cvtColor(cur, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray, scaleFactor=1.1, minNeighbors=1, minSize=(10, 10))
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = cur[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        if len(eyes) == 0:
+            print("Eyes closed")
+        else:
+            print("Eyes open")
+        count += len(eyes)
+        iters += 1
+        if iters >= 2:
+            #iters = 0
+            if count == 0:
+                print("Drowsiness Detected!")
+                status = True
+            else:
+                iters = 0
+            count = 0
+    #idee = files[0]
+    #idee = idee[::-1]
+    #idee = idee[4:24]
+    #idee = idee[::-1]
+    #print(idee)
+    sendPutRequest('LXklRS2NhL5aWVsvxCGn', str(datetime.datetime.now()), status)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
